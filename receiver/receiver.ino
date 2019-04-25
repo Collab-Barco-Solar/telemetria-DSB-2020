d6d4167f953421250bd68e98c0027bd4842e9e05
joaom98 <joaomanoellima98@gmail.com>
2019-04-25 22:19:50 +0000

    Changed the code base to english

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -2,14 +2,14 @@
 #include <LoRa.h>
 #include <Wire.h>  
 
-#define SCK     5    // GPIO5  -- SX1278's SCK
-#define MISO    19   // GPIO19 -- SX1278's MISO
-#define MOSI    27   // GPIO27 -- SX1278's MOSI
-#define SS      18   // GPIO18 -- SX1278's CS
-#define RST     14   // GPIO14 -- SX1278's RESET
-#define DI0     26   // GPIO26 -- SX1278's IRQ(Interrupt Request)
-#define BAND    915E6
-#define BAUD 9600 // baud serial rate
+#define SCK     5     // GPIO5  -- SX1278's SCK
+#define MISO    19    // GPIO19 -- SX1278's MISO
+#define MOSI    27    // GPIO27 -- SX1278's MOSI
+#define SS      18    // GPIO18 -- SX1278's CS
+#define RST     14    // GPIO14 -- SX1278's RESET
+#define DI0     26    // GPIO26 -- SX1278's IRQ(Interrupt Request)
+#define BAND  915E6   // Operating LoRa frequency
+#define BAUD 2000000  // BAUD serial rate
 
 String rssi = "RSSI --";
 String packSize = "--";
@@ -39,8 +39,9 @@ void setup() {
   //LoRa.onReceive(cbk);
   LoRa.receive();
   Serial.println("init ok");
+
+  delay(1000);
   
-  delay(1500);
 }
 
 void loop() {
--- sender/sender.ino
+++ sender/sender.ino
@@ -3,14 +3,15 @@
 #include <Wire.h>  
 
 
-#define SCK     5    // GPIO5  -- SX1278's SCK
-#define MISO    19   // GPIO19 -- SX1278's MISnO
-#define MOSI    27   // GPIO27 -- SX1278's MOSI
-#define SS      18   // GPIO18 -- SX1278's CS
-#define RST     14   // GPIO14 -- SX1278's RESET
-#define DI0     26   // GPIO26 -- SX1278's IRQ(Interrupt Request)
-#define BAND  915E6
-#define BAUD 9600 // baud serial rate
+#define SCK     5     // GPIO5  -- SX1278's SCK
+#define MISO    19    // GPIO19 -- SX1278's MISnO
+#define MOSI    27    // GPIO27 -- SX1278's MOSI
+#define SS      18    // GPIO18 -- SX1278's CS
+#define RST     14    // GPIO14 -- SX1278's RESET
+#define DI0     26    // GPIO26 -- SX1278's IRQ(Interrupt Request)
+#define ANALOG_PIN 36 // GPIO36 -- Analog input
+#define BAND  915E6   // Operating LoRa frequency
+#define BAUD 2000000  // BAUD serial rate
 
 unsigned int counter = 0;
 
@@ -23,43 +24,41 @@ String packet ;
 void setup() {
   pinMode(16,OUTPUT); //oled
   pinMode(2,OUTPUT);
-  
+  pinMode(ANALOG_PIN, INPUT);
   
   Serial.begin(BAUD);
   while (!Serial);
   Serial.println();
   Serial.println("LoRa Sender Test");
   
-  SPI.begin(SCK,MISO,MOSI,SS);
+  SPI.begin(SCK,MISO,MOSI,SS); 
   LoRa.setPins(SS,RST,DI0);
   if (!LoRa.begin(BAND)) {
     Serial.println("Starting LoRa failed!");
     while (1);
   }
   //LoRa.onReceive(cbk);
-//  LoRa.receive();
+  //  LoRa.receive();
   Serial.println("init ok");
- // colocar escrita serial
  
   delay(1500);
 }
 
 void loop() {
 
-  //mostra valor
-  Serial.println(String(counter));
+  double voltage = analogRead(ANALOG_PIN); // Analog voltage measured
 
-
+  //Display counter and analog value in serial monitor
+  Serial.print(String(counter));
+  Serial.println("  tensao> " + String(voltage)); 
   
-  // send packet
+  // Create and send packet
   LoRa.beginPacket();
-  LoRa.print("Telemetria esta avancando> ");
+  LoRa.print("Contador e tensão> ");
   LoRa.print(counter);
+  LoRa.print(" ; ");
+  LoRa.print(voltage);
   LoRa.endPacket();
 
   counter++;
-  digitalWrite(2, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(2, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
 }
