3e51e3750e88528394c614d710ac48219836f92f
joaom98 <joaomanoellima98@gmail.com>
2019-07-05 18:55:34 +0000

    Tweaking GPS

--- GPS/GPS.ino
+++ GPS/GPS.ino
@@ -1,47 +1,90 @@
-#include <TinyGPS++.h>                       
+#include <TinyGPS++.h>
+#include <HardwareSerial.h>
+/*
+   Exemplo de código que usa o Tiny GPS++ pra receber o tempo e localização
+*/
+static const int RXPin = 15, TXPin = 12;
+static const uint32_t GPSBaud = 9600;
 
-#define BAUD 2000000  // BAUD serial rate
+// The TinyGPS++ object
+TinyGPSPlus gps;
 
-TinyGPSPlus gps;                            
-//HardwareSerial Serial1(1);                 
+// The serial connection to the GPS device
+HardwareSerial ss(2);
 
 void setup()
 {
-  Serial.begin(BAUD);
-  Serial1.begin(9600, SERIAL_8N1, 12, 15);   //17-TX 18-RX
+  Serial.begin(115200);
+  ss.begin(GPSBaud, SERIAL_8N1, TXPin, RXPin);
+
+  Serial.println(F("DeviceExample.ino"));
+  Serial.println(F("A simple demonstration of TinyGPS++ with an attached GPS module"));
+  Serial.print(F("Testing TinyGPS++ library v. ")); Serial.println(TinyGPSPlus::libraryVersion());
+  Serial.println(F("by Mikal Hart"));
+  Serial.println();
 }
 
 void loop()
 {
-  Serial.print("Latitude  : ");
-  Serial.println(gps.location.lat(), 5);
-  Serial.print("Longitude : ");
-  Serial.println(gps.location.lng(), 4);
-  Serial.print("Satellites: ");
-  Serial.println(gps.satellites.value());
-  Serial.print("Altitude  : ");
-  Serial.print(gps.altitude.feet() / 3.2808);
-  Serial.println("M");
-  Serial.print("Time      : ");
-  Serial.print(gps.time.hour());
-  Serial.print(":");
-  Serial.print(gps.time.minute());
-  Serial.print(":");
-  Serial.println(gps.time.second());
-  Serial.println("**********************");
-
-  smartDelay(1000);                                      
+  // This sketch displays information every time a new sentence is correctly encoded.
+  while (ss.available() > 0)
+    if (gps.encode(ss.read()))
+      displayInfo();
 
   if (millis() > 5000 && gps.charsProcessed() < 10)
-    Serial.println(F("No GPS data received: check wiring"));
+  {
+    Serial.println(F("No GPS detected: check wiring."));
+    while(true);
+  }
 }
 
-static void smartDelay(unsigned long ms)                
+void displayInfo()
 {
-  unsigned long start = millis();
-  do
+  Serial.print(F("Location: ")); 
+  if (gps.location.isValid())
   {
-    while (Serial1.available())
-      gps.encode(Serial1.read());
-  } while (millis() - start < ms);
+    Serial.print(gps.location.lat(), 6);
+    Serial.print(F(","));
+    Serial.print(gps.location.lng(), 6);
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.print(F("  Date/Time: "));
+  if (gps.date.isValid())
+  {
+    Serial.print(gps.date.month());
+    Serial.print(F("/"));
+    Serial.print(gps.date.day());
+    Serial.print(F("/"));
+    Serial.print(gps.date.year());
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.print(F(" "));
+  if (gps.time.isValid())
+  {
+    if (gps.time.hour() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.hour());
+    Serial.print(F(":"));
+    if (gps.time.minute() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.minute());
+    Serial.print(F(":"));
+    if (gps.time.second() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.second());
+    Serial.print(F("."));
+    if (gps.time.centisecond() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.centisecond());
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.println();
 }
