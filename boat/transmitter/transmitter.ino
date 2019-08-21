79100dd3e1965578f5b12a9f0059b17c4ae646d1
joaom98 <joaomanoellima98@gmail.com>
2019-08-21 20:04:24 -0300

    Added readings and functions

--- boat/transmitter/transmitter.ino
+++ boat/transmitter/transmitter.ino
@@ -5,6 +5,7 @@
 #include <Wire.h>
 #include <TinyGPS++.h>
 #include <HardwareSerial.h>
+#include <Adafruit_ADS1015.h>
 
 
 //pinos
@@ -29,6 +30,12 @@
 #define TXPOWER 20    // Operating LoRa Transmition Power
 #define BAUD 2000000  // BAUD serial rate
 
+#define PIN_POTENCIOMETER 36
+
+#define SHUNT    0x48  // Address of the ADC measuring the motor current
+#define MODULES  0x49 // Address of the ADC on the solar panel modules
+#define BATTERY  0x4A // Address of the ADC on the battery
+
 //objetos
 
 String rssi = "RSSI --";
@@ -37,34 +44,36 @@ String packet ;
 
 TinyGPSPlus gps;      // GPS object
 HardwareSerial ss(2); // Hardware Serial comunication object
-//falta o ads
+
+Adafruit_ADS1115 ads_shunt(SHUNT);
+Adafruit_ADS1115 ads_modules(MODULES);
+Adafruit_ADS1115 ads_battery(BATTERY);
+
+
+
+
+  //   Cheatsheet                                                     ADS1115
+  //                                                                  -------
+  // ads.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit =   0.1875mV (default)
+  // ads.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit =   0.125mV
+  // ads.setGain(GAIN_TWO);        // 2x gain   +/- 2.048V  1 bit =   0.0625mV
+  // ads.setGain(GAIN_FOUR);       // 4x gain   +/- 1.024V  1 bit =   0.03125mV
+  // ads.setGain(GAIN_EIGHT);      // 8x gain   +/- 0.512V  1 bit =   0.015625mV
+  // ads.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit =   0.0078125mV
 
 
 //setup
 
 //functions
 
-String LatitudeGPS( TinyGPSPlus gps ){
-  return "To Implement";
-  /* 
-   while (ss.available() > 0)
-    if (gps.encode(ss.read())){
-      if (gps.location.isValid()){
-        
-        LoRa.print(gps.location.lat(), 6);
-        LoRa.print(F(","));
-        LoRa.print(gps.location.lng(), 6);
-        csvSeparator();
-      }
-      else{
-        GPSerrorMessage();
-        Serial.print("Invalid location");
-      }
-      */
+double LatitudeGPS( ){
+
+  return gps.location.lat();
 }
 
-String LongitudeGPS( TinyGPSPlus gps ){
-  return "To Implement";
+double LongitudeGPS( ){
+  
+  return gps.location.lng();
 }
 
 char CSV_Separator(){
@@ -72,25 +81,46 @@ char CSV_Separator(){
 }
 
 int16_t ShuntRead(){
-
-  return 0;
+  ads_shunt.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit =   0.0078125mV
+  
+  return ads_shunt.readADC_Differential_0_1();
 }
 
 int16_t BatteryRead(){
-
-  return 0;
+  ads_battery.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit =   0.125mV
+  
+  return ads_battery.readADC_Differential_0_1();
 }
 
 int16_t ModulesRead(){
+  ads_modules.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit =   0.125mV
+  
+  return ads_modules.readADC_Differential_0_1();
+}
+
+float PotentiometerRead(){
 
   return 0;
 }
+
+boolean LeftPumpRead(){
+
+  return 0;
+}
+
+boolean RightPumpRead(){
+
+  return 0;
+}
+
 
 
 void setup() {
   
   Serial.begin(BAUD);
   ss.begin(GPSBaud, SERIAL_8N1, TXPin, RXPin); // GPS serial communication
+
+  pinMode(PIN_POTENCIOMETER, INPUT);
   
   while (!Serial);
   Serial.println();
@@ -108,6 +138,7 @@ void setup() {
   LoRa.setSignalBandwidth(BAND);
   LoRa.setTxPower(TXPOWER);
   LoRa.enableCrc();
+
   
   //LoRa.onReceive(cbk);
   //  LoRa.receive();
@@ -120,20 +151,35 @@ void loop() {
   
   // Create and send packet
 
-  // Add Update GPS
-
+  // Update GPS
+  gps.encode(ss.read());
   
   LoRa.beginPacket();
-  // Escrever localizacao
-  LoRa.print(LatitudeGPS(gps));
+  // Write GPS Latitude
+  LoRa.print(LatitudeGPS());
   LoRa.print(CSV_Separator());
-  LoRa.print(LongitudeGPS(gps));
+  // Write GPS Longitude
+  LoRa.print(LongitudeGPS());
   LoRa.print(CSV_Separator());
+  // Write Shunt reading (motor current)
   LoRa.print(ShuntRead());
+  LoRa.print(CSV_Separator());
+  // Write Battery voltage (probably using the ADS1115)
   LoRa.print(BatteryRead());
+  LoRa.print(CSV_Separator());
+  // Write Solar Modules voltage  (probably using the ADS1115)
   LoRa.print(ModulesRead());
+  LoRa.print(CSV_Separator());
+  // Write the potentiometer state that controls the motor 
+  LoRa.print(PotentiometerRead());
+  LoRa.print(CSV_Separator());
+  // Write left pump state
+  LoRa.print(LeftPumpRead());
+  LoRa.print(CSV_Separator());
+  // Write right pump state
+  LoRa.print(RightPumpRead());
+  LoRa.print(CSV_Separator());
   
-  // Escrever o valor no shunt 
 
   
   //LoRa.print(counter);
