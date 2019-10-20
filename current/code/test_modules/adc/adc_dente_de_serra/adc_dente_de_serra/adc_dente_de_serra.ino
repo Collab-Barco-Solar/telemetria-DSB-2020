af7b6d41035eb5ea94fad42e1e5ff2eaf6e90dfc
joaoagricio <joaoagricio@users.noreply.github.com>
2019-10-19 22:08:25 -0300

    logs ADC reads
    Different kinds of inputs in adc pin.

--- current/code/test_modules/adc/adc_dente_de_serra/adc_dente_de_serra/adc_dente_de_serra.ino
+++ current/code/test_modules/adc/adc_dente_de_serra/adc_dente_de_serra/adc_dente_de_serra.ino
@@ -4,28 +4,32 @@
 #define   BAUD 2000000
 #define   pin 4
 #define   DAC1 25
+#define   dacNormPrecision 0.0039215686274509803921568627451  
 
 int logicLevelADC;
 float voltageADC;
+double i = 0;
 
 void setup() {
-  // put your setup code here, to run once:
+  
   Serial.begin(BAUD);
   pinMode(pin, INPUT);
   /* aguarda a serial estar disponível */
   while (!Serial);
   Serial.println();  
 }
-//double passo = 0.00392156863;
-double i = 0;
+
 void loop() {
 
     i > 1 ? i = 0 : 1;
     dacWrite( DAC1 , i*255 );
     Serial.print(i*4095);  
-    i += 0.001;
+    i += dacNormPrecision;
     Serial.print(" ; ");
     Serial.print(analogRead(pin));
+    Serial.print(" ; ");
+    Serial.print(i,8);
     Serial.println(" ; ");
-    delay(50);
+    delay(10);
+    
 }
