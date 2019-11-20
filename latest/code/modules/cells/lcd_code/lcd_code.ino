8819a8b17f7fbe52a1ab99be78f06b482030a1ce
joao agricio <joaoagriciolopes@gmail.com>
2019-11-19 21:12:25 -0300

    up

--- latest/code/modules/cells/lcd_code/lcd_code.ino
+++ latest/code/modules/cells/lcd_code/lcd_code.ino
@@ -11,10 +11,7 @@
 // set the LCD number of columns and rows
 int lcdColumns = 20;
 int lcdRows = 4;
-char first_line[10];
-char second_line[10];
-char third_line[10];
-char fourth_line[10];
+
 // set LCD address, number of columns and rows
 // if you don't know your display address, run an I2C scanner sketch
 LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);  
@@ -30,11 +27,13 @@ void loop(){
   // set cursor to first column, first row
   lcd.setCursor(0, 0);
   // print message
-  lcd.print("Baterias"); 
+  lcd.print("Hello, World!");
+  delay(1000);
+  // clears the display to print new message
+  lcd.clear();
+  // set cursor to first column, second row
   lcd.setCursor(0,1);
   lcd.print("Hello, World!");
   delay(1000);
   lcd.clear(); 
 }
-
-
